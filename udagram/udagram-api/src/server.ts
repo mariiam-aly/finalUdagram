639494e2470e2f47b1ced76f0f2a6f056b2fd310
mariiam-aly <xmairam.aly@gmail.com>
2023-01-10 22:51:12 +0200

    commit

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,9 +25,9 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.PORT || 8080;
 
-  app.use(bodyParser.json());
+  app.use(cors());
 
   // app.use(cors());
   // We set the CORS origin to * so that we don't need to
